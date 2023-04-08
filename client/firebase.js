b2c032d39c4550e1bdb5e80dbdd8b8d4da288f57
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-08 11:46:19 +0530

    update

--- client/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAlK8mFS_6KJtANyEyRgRW610eRqyG8r0g",
-    authDomain: "asksaabi.firebaseapp.com",
-    projectId: "asksaabi",
-    storageBucket: "asksaabi.appspot.com",
-    messagingSenderId: "500426059273",
-    appId: "1:500426059273:web:e9f724627ceacb884692d8"
-  };
