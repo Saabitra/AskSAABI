e36a1ce5f9468e0143d4e912eaace5512db95d4e
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-14 13:28:57 +0530

    update

--- server/server.js
+++ server/server.js
@@ -3,15 +3,12 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
 
-
-
 dotenv.config();
 
 console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
-    apiKey: "process.env.OPENAI_API_KEY",
-    headers: { Authorization: `Bearer "process.env.OPENAI_API_KEY"` },
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -19,6 +16,11 @@ const openai = new OpenAIApi(configuration);
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Set a delay between each request in milliseconds
+const REQUEST_DELAY_MS = 1000;
+
+let lastRequestTime = Date.now();
 
 app.get('/', async(req, res) => {
     res.status(200).send({
@@ -30,15 +32,26 @@ app.post('/', async(req, res) => {
     try {
         const prompt = req.body.prompt;
 
+        // Calculate the time since the last request was made
+        const currentTime = Date.now();
+        const timeSinceLastRequest = currentTime - lastRequestTime;
+        
+        // If the last request was made less than the delay time ago, wait for the remaining time
+        if (timeSinceLastRequest < REQUEST_DELAY_MS) {
+            await new Promise(resolve => setTimeout(resolve, REQUEST_DELAY_MS - timeSinceLastRequest));
+        }
+
+        // Make the API request and update the last request time
         const response = await openai.createCompletion({
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0.5,
-            max_tokens: 3000,
+            max_tokens: 1000,
             top_p: 1,
             frequency_penalty: 0.3,
             presence_penalty: 0,
         });
+        lastRequestTime = Date.now();
 
         res.status(200).send({
             bot: response.data.choices[0].text
